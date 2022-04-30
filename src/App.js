9decbfb4054b3c32cee1ca70373bec189d7ec7ee
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-01 04:01:47 +0600

    notfound added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import NotFound from './components/Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-6xl" >HomePage</h1>
+      <h1 className="text-6xl" >HomePage</h1>
+      <NotFound />
     </div>
   );
 }
---
+++ src/components/Pages/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import error from '../../images/notfound.jpg'
+
+
+const NotFound = () => {
+  return (
+    <div>
+      <img src={error} alt="" />
+    </div>
+  );
+};
+
+export default NotFound;
