b05371962fb286bde90f4ca1efa5e17e1d93e1db
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-06 16:59:02 +0600

    delivered button worked

--- src/components/Pages/BookDetails/BookDetails.js
+++ src/components/Pages/BookDetails/BookDetails.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import UseBookDetails from '../hooks/UseBookDetails';
+import React from "react";
+import { useParams } from "react-router-dom";
+import UseBookDetails from "../hooks/UseBookDetails";
 
 const BookDetails = () => {
   const { bookId } = useParams();
   const [book, setBook] = UseBookDetails(bookId);
- 
 
+  const updateQuantity = (e) => {
+    const quantity = book.quantity - 1;
+    const url = `http://localhost:5000/books/${bookId}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ quantity: quantity }),
+    }).then(() => {
+      setBook({ ...book, quantity: quantity });
+    });
+  };
 
-  
   return (
     <div>
       <div className="grid lg:grid-cols-2 h-full w-full">
@@ -32,7 +43,12 @@ const BookDetails = () => {
             <span className="font-bold">Quantity </span> : {book.quantity}
           </p>
 
-          <button className="btn btn-primary">Delivered</button>
+          <button
+            onClick={() => updateQuantity(book.quantity)}
+            className="btn btn-primary my-4"
+          >
+            Delivered
+          </button>
         </div>
       </div>
     </div>
