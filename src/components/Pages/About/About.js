2a9d67f5bc33b5d7ebad6e9e34d34b2668023df1
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-08 02:26:21 +0600

    another section polished

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -8,30 +8,30 @@ import stra from "../../images/strategy.png";
 const About = () => {
   return (
     <div className="mb-20">
-      <div className="container mt-6 bg-sky-50 py-14">
+      <div className="container mt-6 bg-violet-50 py-14">
         <p className="mt-8 text-2xl font-semibold mb-4 font-serif   text-center ">
-        WE WORK HARD, WE PLAY HARD
+          WE WORK HARD, WE PLAY HARD
         </p>
         <h1 className="text-center text-4xl font-bold italic font-serif  mb-16 ">
-           WHY WORK WITH US <span className="text-red-600" >?</span>
+          WHY WORK WITH US <span className="text-red-600">?</span>
         </h1>
 
         <div className="  ">
           <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-10 mt-10 mb-20 ">
             <div className="mx-auto bg-slate-300	 rounded	 pt-8 px-8">
-              <img className="rounded" src={advice} alt="" />
+              <img className="rounded-full" src={advice} alt="" />
               <p className="text-center text-xl font-serif p-6 "> Advisory</p>
             </div>
             <div className="mx-auto bg-slate-300 rounded	 pt-8 px-8">
-              <img className="rounded" src={dev} alt="" />
+              <img className="rounded-full" src={dev} alt="" />
               <p className="text-center text-xl font-serif p-6">Development</p>
             </div>
             <div className="mx-auto bg-slate-300 rounded	 pt-8 px-8">
-              <img src={pln} alt="" />
+              <img className="rounded-full" src={pln} alt="" />
               <p className="text-center text-xl font-serif p-6">Planning</p>
             </div>
             <div className="mx-auto bg-slate-300 rounded	 pt-8 px-8">
-              <img src={stra} alt="" />
+              <img className="rounded-full" src={stra} alt="" />
               <p className="text-center text-xl font-serif p-6">Strategy</p>
             </div>
           </div>
